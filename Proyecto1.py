4352c787a8924684fa1f0a0bf59f15c8f0bfcc78
DennysGO <73663572+dennys0405@users.noreply.github.com>
2024-04-18 23:40:44 -0500

    Update Proyecto1.py

--- Proyecto1.py
+++ Proyecto1.py
@@ -11,20 +11,3 @@ import numpy as np
 
 st.title('Monitoreo de calidad de aire QAIRA:')
 st.write('[Municipalidad de Miraflores]')
-num = st.slider("num", 0, 100, step=1)
-st.write("El numero ingresado es {}".format(num))
-
-option = st.selectbox(
- '¿Cómo desearía ser contactado/a?',
- ('Email', 'Teléfono', 'Whatsapp'))
-st.write('Seleccionó:', option)
-
-
-n = st.slider("n", 5,100, step=1)
-chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
-st.line_chart(chart_data)
-
-df = pd.DataFrame(
- np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
- columns=['lat', 'lon'])
-st.map(df)
