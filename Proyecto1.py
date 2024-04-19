2551bac7b64f906889f54a40b9a60de83b51da13
DennysGO <73663572+dennys0405@users.noreply.github.com>
2024-04-18 23:47:43 -0500

    Update Proyecto1.py

--- Proyecto1.py
+++ Proyecto1.py
@@ -8,13 +8,33 @@ Created on Thu Apr 18 21:29:23 2024
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-st.title('Monitoreo de calidad de aire QAIRA:')
-st.write('[Municipalidad de Miraflores]')
-
+import pandas as pd
+import plotly.express as px
 
 
-df8 = pd.read_excel('8_Monitoreo_Enero_2021.xlsx')
+# Cargar los conjuntos de datos mensuales
+df_ene = pd.read_csv('datos_enero_2021.csv')
+df_feb = pd.read_csv('datos_febrero_2021.csv')
+df_mar = pd.read_csv('datos_marzo_2021.csv')
+df_abr = pd.read_csv('datos_abril_2021.csv')
+df_may = pd.read_csv('datos_mayo_2021.csv')
+df_jun = pd.read_csv('datos_junio_2021.csv')
 
-df=pd.concat([df8], axis=0, ignore_index=True)
-df
+# Convertir la columna de fecha a tipo datetime y eliminar las horas
+df_ene['Fecha'] = pd.to_datetime(df_ene['Fecha']).dt.date
+df_feb['Fecha'] = pd.to_datetime(df_feb['Fecha']).dt.date
+df_mar['Fecha'] = pd.to_datetime(df_mar['Fecha']).dt.date
+df_abr['Fecha'] = pd.to_datetime(df_abr['Fecha']).dt.date
+df_may['Fecha'] = pd.to_datetime(df_may['Fecha']).dt.date
+df_jun['Fecha'] = pd.to_datetime(df_jun['Fecha']).dt.date
+
+# Unir los conjuntos de datos en uno solo
+df = pd.concat([df_ene, df_feb, df_mar, df_abr, df_may, df_jun])
+
+# Crear gráficos de líneas para NO2 y PM10
+fig = px.line(df, x='Fecha', y=['NO2 (ug/m3)', 'PM10 \n(ug/m3)'], title='Evolución de NO2 y PM10 de enero a junio de 2021',
+              labels={'Fecha': 'Fecha', 'value': 'Concentración (ug/m3)', 'variable': 'Contaminante'},
+              template='plotly_dark')
+
+# Mostrar el gráfico
+fig.show()
